d4e1a55ace2007137e2496aebb97defcff8d043a
hexg8236 <hexg8236@163.com>
2024-02-18 16:31:37 +0800

    首页-涨停榜，涨停板接口功能开发

--- stock_backend/src/main/java/top/newhand/stock/service/impl/StockServiceImpl.java
+++ stock_backend/src/main/java/top/newhand/stock/service/impl/StockServiceImpl.java
@@ -1,5 +1,7 @@
 package top.newhand.stock.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +9,22 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import top.newhand.stock.mapper.StockBlockRtInfoMapper;
 import top.newhand.stock.mapper.StockMarketIndexInfoMapper;
+import top.newhand.stock.mapper.StockRtInfoMapper;
 import top.newhand.stock.pojo.domain.InnerMarketDomain;
 import top.newhand.stock.pojo.domain.StockBlockDomain;
+import top.newhand.stock.pojo.domain.StockUpdownDomain;
+import top.newhand.stock.pojo.domain.StockUpdownListDomain;
 import top.newhand.stock.pojo.vo.StockInfoConfig;
 import top.newhand.stock.service.StockService;
 import top.newhand.stock.utils.DateTimeUtil;
 import top.newhand.stock.vo.R;
 import top.newhand.stock.vo.ResponseCode;
+import top.newhand.stock.vo.resp.PageResult;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName StockServiceImpl
@@ -44,12 +52,21 @@ public class StockServiceImpl implements StockService {
     private StockMarketIndexInfoMapper stockMarketIndexInfoMapper;
 
     /**
+     * @Description 股票涨跌信息数据接口
+     * @Param 
+     * @Date 14:25 2024/2/18
+     **/
+    @Autowired
+    private StockRtInfoMapper stockRtInfoMapper;
+
+    /**
      * @Description 股票板块数据接口
      * @Param
      * @Date 13:44 2024/2/18
      **/
     @Autowired
     private StockBlockRtInfoMapper stockBlockRtInfoMapper;
+
     @Override
     public R<List<InnerMarketDomain>> innerIndexAll() {
         // 1、获取国内A股的大盘集合
@@ -63,6 +80,11 @@ public class StockServiceImpl implements StockService {
         return R.ok(marketInfo);
     }
 
+    /**
+     * @Description 查询板块指数服务
+     * @Param []
+     * @Date 14:29 2024/2/18
+     **/
     @Override
     public R<List<StockBlockDomain>> sectorAllLimit() {
         //获取股票最新交易时间点
@@ -75,5 +97,75 @@ public class StockServiceImpl implements StockService {
             return R.error(ResponseCode.NO_RESPONSE_DATA.getMessage());
         }
         return R.ok(stockBlockDomains);
+    }
+
+    @Override
+    public R<PageResult> getStockPageInfo(Integer page, Integer pageSize) {
+        // 1、 设置PageHelper 分页参数
+        PageHelper.startPage(page, pageSize);
+        // 2、根据当前最新的股票交易时间地点
+        Date curDate = DateTimeUtil.getLastDate4Stock(DateTime.now()).toDate();
+        // TODO mock数据
+        curDate= DateTime.parse("2022-06-07 15:00:00", DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")).toDate();
+        // 3、调用Mapper接口查询
+        List<StockUpdownDomain> infos = stockRtInfoMapper.getNewestStockInfo(curDate);
+        // 判断查询数据是否有响应值
+        if (CollectionUtils.isEmpty(infos)) {
+            return R.error(ResponseCode.NO_RESPONSE_DATA);
+        }
+        //4.组装PageInfo对象，获取分页的具体信息,因为PageInfo包含了丰富的分页信息，而部分分页信息是前端不需要的
+        //PageInfo<StockUpdownDomain> pageInfo = new PageInfo<>(infos);
+        //PageResult<StockUpdownDomain> pageResult = new PageResult<>(pageInfo);
+        PageResult<StockUpdownDomain> pageResult = new PageResult<>(new PageInfo<>(infos));
+        return R.ok(pageResult);
+    }
+
+    /**
+     * @Description 查询涨幅榜信息
+     * @Param []
+     * @Date 14:52 2024/2/18
+     **/
+    @Override
+    public R<List<StockUpdownListDomain>> getStockUpdownList() {
+        //获取股票最新交易时间点
+        Date lastDate = DateTimeUtil.getLastDate4Stock(DateTime.now()).toDate();
+        //TODO mock数据,后续删除
+        lastDate=DateTime.parse("2021-12-30 10:30:00", DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")).toDate();
+        List<StockUpdownListDomain> infos = stockRtInfoMapper.getStockUpdownList(lastDate);
+        if (CollectionUtils.isEmpty(infos)) {
+            return R.error(ResponseCode.NO_RESPONSE_DATA.getMessage());
+        }
+        return R.ok(infos);
+    }
+
+    /**
+     * @Description 统计最新交易日下股票每分钟涨跌停的数量
+     * @Param []
+     * @Date 16:20 2024/2/18
+     **/
+    @Override
+    public R<Map> getStockUpdownCount() {
+        //1.获取最新的交易时间范围 openTime  curTime
+        //1.1 获取最新股票交易时间点
+        DateTime curDateTime = DateTimeUtil.getLastDate4Stock(DateTime.now());
+        Date curTime = curDateTime.toDate();
+        //TODO mock 数据
+        curTime= DateTime.parse("2022-01-06 14:25:00", DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")).toDate();
+        //1.2 获取最新交易时间对应的开盘时间
+        DateTime openDate = DateTimeUtil.getOpenDate(curDateTime);
+        Date openTime = openDate.toDate();
+        //TODO mock数据
+        openTime= DateTime.parse("2022-01-06 09:30:00", DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")).toDate();
+        //2.查询涨停数据
+        //约定mapper中flag入参： 1-》涨停数据 0：跌停
+        List<Map> upCounts=stockRtInfoMapper.getStockUpdownCount(openTime,curTime,1);
+        //3.查询跌停数据
+        List<Map> dwCounts=stockRtInfoMapper.getStockUpdownCount(openTime,curTime,0);
+        //4.组装数据
+        HashMap<String, List> mapInfo = new HashMap<>();
+        mapInfo.put("upList",upCounts);
+        mapInfo.put("downList",dwCounts);
+        //5.返回结果
+        return R.ok(mapInfo);
     }
 }
--- stock_common/src/main/java/top/newhand/stock/mapper/StockRtInfoMapper.java
+++ stock_common/src/main/java/top/newhand/stock/mapper/StockRtInfoMapper.java
@@ -1,6 +1,13 @@
 package top.newhand.stock.mapper;
 
+import org.apache.ibatis.annotations.Param;
+import top.newhand.stock.pojo.domain.StockUpdownDomain;
+import top.newhand.stock.pojo.domain.StockUpdownListDomain;
 import top.newhand.stock.pojo.entity.StockRtInfo;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 /**
 * @author hexg8
@@ -22,4 +29,24 @@ public interface StockRtInfoMapper {
 
     int updateByPrimaryKey(StockRtInfo record);
 
+    /**
+     * @Description 查询指定时间点下股票的数据，并按照涨幅降序排序
+     * @Param [curDate]
+     * @Date 14:32 2024/2/18
+     **/
+    List<StockUpdownDomain> getNewestStockInfo(@Param("timePoint") Date curDate);
+
+    /**
+     * @Description 查询涨幅榜数据
+     * @Param [lastDate]
+     * @Date 14:51 2024/2/18
+     **/
+    List<StockUpdownListDomain> getStockUpdownList(@Param("timePoint") Date lastDate);
+
+    /**
+     * @Description 查询指定时间范围内每分钟涨停或者跌停的数量
+     * @Param [openTime 开始时间，, curTime结束时间，  flag 约定 1-> 涨停， 0 -> 跌停]
+     * @Date 16:22 2024/2/18
+     **/
+    List<Map> getStockUpdownCount(@Param("openTime") Date openTime, @Param("curTime") Date curTime, @Param("flag") int flag);
 }
